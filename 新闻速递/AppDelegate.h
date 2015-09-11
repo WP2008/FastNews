e96c8a350632b6b0f8b7562feffce47ebadde0fc
wp <wp1991wp.163.com>
2015-09-11 09:20:29 +0800

    Initial Commit

---
+++ 新闻速递/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  新闻速递
+//
+//  Created by tarena on 15/9/11.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 新闻速递/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  新闻速递
+//
+//  Created by tarena on 15/9/11.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
